bd28855f992a2f91be868f0aba23ee0f75587e06
GOD_OF_WOR <ayxanceferov2002ac@gmail.com>
2023-12-14 10:41:12 +0400

    js-dom-6-2

---
+++ app.js
@@ -0,0 +1,12 @@
+let btns = document.querySelectorAll(".btn");
+
+let div = document.querySelector(".content");
+
+btns.forEach((btn) => {
+    btn.addEventListener("click", function () {
+        let color = getComputedStyle(btn).backgroundColor;
+        div.style.backgroundColor = color;
+    }
+    )
+}
+)
